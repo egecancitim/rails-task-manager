2d97ab808b1998fee972b9c07dfa91e1a0cf8aaf
Egecan Citim <egecancitim@gmail.com>
2020-04-07 14:56:52 +0300

    taskmanager completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # see all the tasks
+  # get "tasks", to: "tasks#index"
+  #   # create new task
+  # get "tasks/new", to: "tasks#new"
+  # # see one task
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create"
+  # # edit and update one specfic task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+
+  # # delete one specific task
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
